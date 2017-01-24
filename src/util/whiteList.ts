c09c0bda8a24d7de0fec2a7c7dee28692f1adde6
e-cloud <saintscott119@gmail.com>
2017-01-24 19:00:56 +0800

    feat: white list test

---
+++ src/util/whiteList.ts
@@ -0,0 +1,11 @@
+// from http://stackoverflow.com/a/24558913/3326749
+export function glob(pattern, input) {
+  const re = new RegExp(pattern.replace(/([.?+^$[\]\\(){}|\/-])/g, "\\$1").replace(/\*/g, '.*'));
+  return re.test(input);
+}
+
+export function genTester(pattern){
+  return function (input) {
+    return glob(pattern, input)
+  }
+}
